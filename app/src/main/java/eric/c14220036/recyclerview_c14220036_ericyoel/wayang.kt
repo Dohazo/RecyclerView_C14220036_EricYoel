fcb5199eecd39e350266a36074afa2e487cf1dfd
Noflus101 <pootisspy931@gmail.com>
2024-11-05 20:37:18 +0700

    Initial commit by Eric Yoel

---
+++ app/src/main/java/eric/c14220036/recyclerview_c14220036_ericyoel/wayang.kt
@@ -0,0 +1,8 @@
+package eric.c14220036.recyclerview_c14220036_ericyoel
+
+data class wayang(
+    var foto : String,
+    var nama : String,
+    var karakter : String,
+    var deskripsi : String
+)
