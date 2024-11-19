33d52b680f11dc8be98786f1907ff831397366f4
Noflus101 <pootisspy931@gmail.com>
2024-11-19 19:51:43 +0700

    10.04. RecyclerView - Hapus Data

--- app/src/main/java/eric/c14220036/recyclerview_c14220036_ericyoel/MainActivity.kt
+++ app/src/main/java/eric/c14220036/recyclerview_c14220036_ericyoel/MainActivity.kt
@@ -1,9 +1,11 @@
 package eric.c14220036.recyclerview_c14220036_ericyoel
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -13,10 +15,10 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var _nama : Array<String>
-    private lateinit var _karakter : Array<String>
-    private lateinit var _deskripsi : Array<String>
-    private lateinit var _gambar : Array<String>
+    private lateinit var _nama : MutableList<String>
+    private lateinit var _karakter :  MutableList<String>
+    private lateinit var _deskripsi :  MutableList<String>
+    private lateinit var _gambar :  MutableList<String>
 
     private lateinit var _rvWayang : RecyclerView
     private  var arWayang = arrayListOf<wayang>()
@@ -35,12 +37,13 @@ class MainActivity : AppCompatActivity() {
         tampilkanData()
     }
     fun siapkanData(){
-        _nama = resources.getStringArray(R.array.namaWayang)
-        _deskripsi = resources.getStringArray(R.array.deskripsiWayang)
-        _karakter = resources.getStringArray(R.array.karakterUtamaWayang)
-        _gambar = resources.getStringArray(R.array.gambarWayang)
+        _nama = resources.getStringArray(R.array.namaWayang).toMutableList()
+        _deskripsi = resources.getStringArray(R.array.deskripsiWayang).toMutableList()
+        _karakter = resources.getStringArray(R.array.karakterUtamaWayang).toMutableList()
+        _gambar = resources.getStringArray(R.array.gambarWayang).toMutableList()
     }
     fun tambahdata(){
+        arWayang.clear()
         for(position in _nama.indices){
             val data = wayang(
                 _gambar[position],
@@ -69,6 +72,33 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this@MainActivity, detWayang::class.java)
                 intent.putExtra("kirimData",data)
                 startActivity(intent)
+            }
+
+            override fun delData(pos: Int) {
+                AlertDialog.Builder(this@MainActivity)
+                    .setTitle("Hapus Data")
+                    .setMessage("Apakah Benar Data " + _nama[pos] + " akan dihapus ?")
+                    .setPositiveButton(
+                        "HAPUS",
+                        DialogInterface.OnClickListener{dialog, which ->
+                            _gambar.removeAt(pos)
+                            _nama.removeAt(pos)
+                            _deskripsi.removeAt(pos)
+                            _karakter.removeAt(pos)
+                            tambahdata()
+                            tampilkanData()
+                        }
+                    )
+                    .setNegativeButton(
+                        "BATAL",
+                        DialogInterface.OnClickListener{dialog,which ->
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Data Batal Dihapus",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    ).show()
             }
         })
 
--- app/src/main/java/eric/c14220036/recyclerview_c14220036_ericyoel/adapterRevView.kt
+++ app/src/main/java/eric/c14220036/recyclerview_c14220036_ericyoel/adapterRevView.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -16,6 +17,7 @@ class adapterRevView(private val listWayang: ArrayList<wayang>) : RecyclerView.A
 
     interface OnItemClickCallback {
         fun onItemClicked(data:wayang)
+        fun delData(pos:Int)
     }
 
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback){
@@ -26,6 +28,7 @@ class adapterRevView(private val listWayang: ArrayList<wayang>) : RecyclerView.A
         var _karakterWayang = itemView.findViewById<TextView>(R.id.karakterWayang)
         var _deskripsiWayang = itemView.findViewById<TextView>(R.id.deskripsiWayang)
         var _gambarWayang = itemView.findViewById<ImageView>(R.id.gambarWayang)
+        var _btnHapus = itemView.findViewById<Button>(R.id.btnHapus)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -50,6 +53,9 @@ class adapterRevView(private val listWayang: ArrayList<wayang>) : RecyclerView.A
         holder._gambarWayang.setOnClickListener {
 //            Toast.makeText(holder.itemView.context,wayang.nama,Toast.LENGTH_LONG).show()
             onItemClickCallback.onItemClicked(listWayang[position])
+        }
+        holder._btnHapus.setOnClickListener {
+            onItemClickCallback.delData(position)
         }
     }
 }
